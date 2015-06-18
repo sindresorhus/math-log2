4141df6afc88208372d56abf4165a0b7fada8388
Sindre Sorhus <sindresorhus@gmail.com>
2015-06-18 16:36:57 +0200

    use `float-equal` for tests

--- test.js
+++ test.js
@@ -2,12 +2,9 @@
 var test = require('ava');
 var numberIsNan = require('number-is-nan');
 var positiveZero = require('positive-zero');
+var floatEqual = require('float-equal');
 Math.mathLog2 = undefined;
 var mathLog2 = require('./');
-
-function epsilon(a, b, E) {
-	return Math.abs(a - b) <= (E ? E : 1e-11);
-};
 
 test(function (t) {
 	t.assert(numberIsNan(mathLog2(NaN)));
@@ -18,6 +15,6 @@ test(function (t) {
 	t.assert(mathLog2(-0) === -Infinity);
 	t.assert(mathLog2(0.5) === -1);
 	t.assert(mathLog2(32) === 5);
-	t.assert(epsilon(mathLog2(5), 2.321928094887362));
+	t.assert(floatEqual(mathLog2(5), 2.321928094887362));
 	t.end();
 });
